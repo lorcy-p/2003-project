3c07555eebb54e694f36a270cad62946b6fe1851
Joshua Cavanagh <joshua.cavanagh@students.plymouth.ac.uk>
2025-03-07 16:45:07 +0000

    Updating AI selection screen
    I have began tweaking the AI selection screen.
    
    I have changed the layout to provide a selection box as well as an overview section of the scenario selected.
    
    I am still working on the colour scheme and layout for the overview box.
    
    A welcome message will be displayed for now while no scenario is selected.

--- src/Screens/SelectAiScreen.tsx
+++ src/Screens/SelectAiScreen.tsx
@@ -17,27 +17,60 @@ const SelectAI: React.FC = () => {
     }
 
     return (
-    <div className="container">
         
-        <div className="title">
-            <span className="main-text">Metaphysical</span>
-            <span className="sub-text">Studio</span>
-        </div>
-        <div className="ai_container">
-            <h1 className="chatting_text">Start Chatting</h1>
-            <div className="ai_selector_container">
+        <div className="container">
+
+            <div className="title">
+                <span className="main-text">Metaphysical</span>
+                <span className="sub-text">Studio</span>
+            </div>
+            <div className="ai_container">
 
                 <div className={`select_ai ${isClicked? "clicked" : "not_clicked"}`} onClick={() => setIsClicked(!isClicked)} onDoubleClick={() => {setIsClicked(!isClicked); beginChatting();}}>
-
                     <img src="images/AI image.png" alt="AI image" className="ai_image"/>
-                    <p className="ai_text">Talk with John. If you ever need a great mind to talk to John is your guy, he can provide you with a range of insight into different topics</p>
+                    <h1 className="name">Fred the pig farmer</h1>
                 </div>
-                
+
             </div>
-            <button className="begin_button" onClick={beginChatting}>Begin Chat</button>
+
+            <div className="overview_box">
+
+                {isClicked && (
+                    <>
+                    <img src="images/AI image.png" alt="AI image" className="ai_image_overview"/>
+
+                    <div className="ai_details">
+                        <div>
+                            <p>Name: Fred the pig farmer</p>
+                            <p>Age: 32</p>
+                            <p>Residence: Cornwall</p>
+                        </div>
+                    </div>
+
+                    <div className="ai_description">
+                        <div>
+                            <h1>Description:</h1>
+                            <p>Fred is a pig farmer by blood. His love for pig farming started at a very young age and it is all he had dedicated his life to. He can tell you anything you need to know about pigs.</p>
+                        </div>
+                    </div>
+
+                    <button className="begin_button">Begin</button>
+                    
+                    </>
+                )}
+
+                {!isClicked && (
+                    <>
+                        <h1 className="welcome_text">Welcome to Metaphysical Studios' AI chat bot</h1>
+
+                        <p className="welcome_para">Select a range of different scenarios to talk to. We have made use of realistic models to provide and immersive realistic experience for our users. Select a scenario on the left and try it out!</p>
+                    </>
+                )}
+
+            </div>
+
+                
         </div>
-        
-    </div>
   
   );
 };
