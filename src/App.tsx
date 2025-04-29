dcb46efc54265f239497a6fa6c3aa3d975d82f7d
Jass Bokhammas <bokhammas86@gmail.com>
2025-04-29 17:51:42 +0100

    Update App.tsx
    Added protected route example to the characters route.

--- src/App.tsx
+++ src/App.tsx
@@ -9,15 +9,25 @@ import SelectAI from "./Screens/SelectAiScreen";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginScreen from "./Screens/LoginScreen";
 import HomeScreen from "./Screens/HomeScreen";
+import ProtectedRoute from "./components/ProtectedRoute";
+import useAuth from './hooks/useAuth';
 
 const App: FC = () => {
+  const { isAuthenticated, userToken, userId, login, logout } = useAuth();
   return (
     <>
       <Router>
         <Routes>
           <Route path="/login" element={<LoginScreen />} />
           <Route path="/" element={<HomeScreen />} />
-          <Route path="/characters" element={<SelectAI />} />
+            <Route path="/characters" element={
+                <ProtectedRoute
+                    isAuthenticated={isAuthenticated}
+                    authenticationPath="/login"
+                >
+                    <SelectAI />
+                </ProtectedRoute>
+            } />
           <Route path="/chat" element={<ChatScreen />} />
         </Routes>
       </Router>
